38e2add8fb720c1bd5f942c2b346e4fc04469510
herosamson <herosamson10152@gmail.com>
2025-02-11 10:57:39 +0800

    asd

--- server/models/registerUser.js
+++ server/models/registerUser.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const RegisterSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
-  contact: { type: Number, required: true, unique: true },
+  contact: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
